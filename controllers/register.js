cc6ebb4afe78811c955ca889e1cda436187bb3cc
Bibhabasu <bibhabasucvsc@kgpian.iitkgp.ac.in>
2024-03-21 04:17:08 +0530

    api for user registration

---
+++ controllers/register.js
@@ -0,0 +1,55 @@
+const { JWT_SECRET } = process.env;
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const router = require('express').Router();
+const { check, validationResult } = require('express-validator');
+
+
+router.post('/user/register',  // Register user endpoint
+    [check('username', 'username length should be 10 to 30 characters')
+        .isLength({ min: 10, max: 30 }),
+    check('name', 'Name length should be 10 to 20 characters')
+        .isLength({ min: 10, max: 20 }),
+    check('password', 'Password length should be 8 to 10 characters')
+        .isLength({ min: 8, max: 10 })],
+    async (req, res) => {   // Register user controller
+        try {
+
+            const { username, name, password, token } = req.body;  // Get username, name, password and token from request body
+            const decoded = jwt.verify(token, JWT_SECRET);  // Verify token
+
+            const existingUser = await prisma.user.findUnique({  // Check if user already exists
+                where: {
+                    id: decoded.id
+                }
+            });
+
+            if (!existingUser) {
+                return res.status(404).json({ message: "User not found" });
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 10);  // Hash password
+            const updatedUser = await prisma.user.update({  // Update user
+                where: {
+                    id: decoded.id
+                },
+                data: {
+                    username,
+                    name,
+                    password: hashedPassword,
+                }
+            });
+            const newToken = jwt.sign({ id: updatedUser.id }, JWT_SECRET);  // Generate new token or user id
+            res.json({ message: "User updated", data: { token: newToken, user: updatedUser } });  // Return token and user data
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ message: "Internal server error" });
+        }
+    });
+
+
+module.exports = router;
+
+
