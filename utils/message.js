9bab47bd6bf29520913ea44a08e123d65cb5942a
Uday <udayomsrivastava111@gmail.com>
2024-03-22 02:30:45 +0530

    sid change twillio

--- utils/message.js
+++ utils/message.js
@@ -1,5 +1,5 @@
 const accountSid =
-  process.env.TWILIO_ACCOUNT_SID || "MG44d25b2065b170789541a1143718415f";
+  process.env.TWILIO_ACCOUNT_SID || "AC979bb523aaaaf7c718d49305aa5a11bc";
 const authToken =
   process.env.TWILIO_AUTH_TOKEN || "2d66a444045e142ed31374fb7c475357";
 const twilioNumber = process.env.TWILIO_PHONE_NUMBER || "+12513179666";
@@ -11,6 +11,7 @@ async function message({ to, body }) {
     const message = await client.messages.create({
       body: `${body}`,
       from: twilioNumber, // Twilio number
+      messagingServiceSid: process.env.TWILIO_MS_SID || 'MG44d25b2065b170789541a1143718415f',
       to: `${to}`,
     });
 
