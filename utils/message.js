2d644e8246c074f281c791f165767c82d89cc1e7
Uday <udayomsrivastava111@gmail.com>
2024-03-22 02:25:29 +0530

    sid change twillio

--- utils/message.js
+++ utils/message.js
@@ -1,5 +1,5 @@
 const accountSid =
-  process.env.TWILIO_ACCOUNT_SID || "AC979bb523aaaaf7c718d49305aa5a11bc";
+  process.env.TWILIO_ACCOUNT_SID || "MG44d25b2065b170789541a1143718415f";
 const authToken =
   process.env.TWILIO_AUTH_TOKEN || "2d66a444045e142ed31374fb7c475357";
 const twilioNumber = process.env.TWILIO_PHONE_NUMBER || "+12513179666";
