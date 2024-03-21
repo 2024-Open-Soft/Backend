848588dff02b3456a7748d0fcfc32a4ac9b06351
Uday <udayomsrivastava111@gmail.com>
2024-03-22 01:23:01 +0530

    added mongoose schema

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const otpRoutes = require("./routes/otpRoutes");
 
 require("dotenv").config();
 
+// const mongoose = require('mongoose');
+
 const PORT = 3001;
 
 app.use(express.json());
@@ -15,3 +17,8 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
 
+// mongoose.connect(process.env.DATABASE_URL).then(() => {
+//     app.listen(PORT, () => {
+//         console.log(`App listening on port ${PORT}`)
+//     })
+// });.
