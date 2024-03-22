269cdbb20b926db3eca32a9816c009a8d7dc1e07
Uday <udayomsrivastava111@gmail.com>
2024-03-22 22:00:51 +0530

    update and delete from watchlist

---
+++ controllers/movieWatchlistController.js
@@ -0,0 +1,90 @@
+const updateWatchlistController = async (req, res) => {
+    try {
+        const token = parseToken(req);
+        const userId = token.userId;
+
+        const { movieId } = req.body;
+
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId
+            }
+        });
+
+        if (!user) {
+            return res.status(400).json({ message: "User not found" });
+        }
+
+        // find movieId in user.watchlist
+        const watchlist = user.watchlist.find((movie) => movie.movieId === movieId);
+
+        // add that movie to the watchlist
+        if (!watchlist) {
+            user.watchlist.push({ movieId });
+        }
+
+        // update the user's watchlist
+        await prisma.user.update({
+            where: {
+                id: userId
+            },
+            data: {
+                watchlist: user.watchlist
+            }
+        });
+
+        return res.status(200).json({ message: "Watchlist updated" });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+const deleteWatchlistController = async (req, res) => {
+    try {
+        const token = parseToken(req);
+        const userId = token.userId;
+
+        const { movieId } = req.body;
+
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId
+            }
+        });
+
+        if (!user) {
+            return res.status(400).json({ message: "User not found" });
+        }
+
+        // find movieId in user.watchlist
+        const watchlist = user.watchlist.find((movie) => movie.movieId === movieId);
+
+        // remove that movie from the watchlist
+        if (watchlist) {
+            user.watchlist = user.watchlist.filter((movie) => movie.movieId !== movieId);
+        }
+
+        // update the user's watchlist
+        await prisma.user.update({
+            where: {
+                id: userId
+            },
+            data: {
+                watchlist: user.watchlist
+            }
+        });
+
+        return res.status(200).json({ message: "Watchlist updated" });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+module.exports = {
+    updateWatchlistController,
+    deleteWatchlistController
+}
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 
 const { isLoggedIn } = require('../middleware');
-const { updateHistoryController, deleteHistoryController } = require('../controllers/movieController');
+const { updateHistoryController, deleteHistoryController } = require('../controllers/movieHistoryController');
+const { updateWatchlistController, deleteWatchlistController } = require('../controllers/movieWatchlistController');
 
 
 router.post("/history", isLoggedIn, updateHistoryController);
 router.delete("/history", isLoggedIn, deleteHistoryController);
+
+router.post("/watchlist", isLoggedIn, updateWatchlistController);
+router.delete("/watchlist", isLoggedIn, deleteWatchlistController);
