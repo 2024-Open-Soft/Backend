6b16334d2d8bd586e0318cb360d1be172c1a925c
Uday <udayomsrivastava111@gmail.com>
2024-03-23 00:02:40 +0530

    required removal

--- controllers/adminMovieController.js
+++ controllers/adminMovieController.js
@@ -1,39 +1,11 @@
 const prisma = require('../prisma/index');
 
-const requiredProperties = {
-    id: true,
-    awards: true,
-    cast: true,
-    countries: true,
-    directors: true,
-    fullplot: true,
-    genres: true,
-    imdb: true,
-    languages: true,
-    lastupdated: true,
-    metacritic: true,
-    num_mflix_comments: true,
-    plot: true,
-    plot_embedding: true,
-    poster: true,
-    rated: true,
-    released: true,
-    runtime: true,
-    title: true,
-    tomatoes: true,
-    type: true,
-    writers: true,
-    year: true,
-    comments: true,
-};
-
 const getAllMovies = async (req, res) => {
     try {
         // paginatedResponse, take page number from query params and return 10 movies per page
         const page = req.query.page ? parseInt(req.query.page) : 1;
 
         const movies = await prisma.embedded_movies.findMany({
-            select: requiredProperties,
             skip: (page - 1) * 10,
             take: 10
         });
