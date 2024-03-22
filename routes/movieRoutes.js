5a21236cc4df4928113b7737727d6fc3e23ff59c
Uday <udayomsrivastava111@gmail.com>
2024-03-22 23:16:49 +0530

    validators for movieRoutes.js

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -5,8 +5,92 @@ const { updateHistoryController, deleteHistoryController } = require('../control
 const { updateWatchlistController, deleteWatchlistController } = require('../controllers/movieWatchlistController');
 
 
-router.post("/history", isLoggedIn, updateHistoryController);
-router.delete("/history", isLoggedIn, deleteHistoryController);
 
-router.post("/watchlist", isLoggedIn, updateWatchlistController);
-router.delete("/watchlist", isLoggedIn, deleteWatchlistController);
+const { body, header, oneOf, validationResult, withMessage } = require("express-validator");
+
+// Middleware function to validate email or phoneNumber
+const validateAddToHistory = [
+    // Check if email or phoneNumber is provided
+    oneOf([
+        body("movieId",).exists().isMobilePhone(),
+        [
+            body("email").exists().isEmail(),
+            header("Authorization").exists()
+        ]
+    ], { message: "Email with Authentication or phoneNumber is required" }),
+    // Check for validation errors
+    (req, res, next) => {
+        const errors = validationResult(req);
+        console.log(errors);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    }
+];
+
+
+router.post("/history",
+    [
+        body("movieId", "Movie Id required").exists(),
+        body("timestamp", "Timestamp required").optional().isLength({ min: 5, max: 8 }),
+        header("Authorization", "Authorization token is required").exists()
+    ],
+    (req, res, next) => {
+        const errors = validationResult(req);
+        console.log(errors);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    },
+    isLoggedIn, updateHistoryController
+);
+
+router.delete("/history",
+    [
+        body("movieId", "Movie Id required").exists(),
+        header("Authorization", "Authorization token is required").exists()
+    ],
+    (req, res, next) => {
+        const errors = validationResult(req);
+        console.log(errors);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    },
+    isLoggedIn, deleteHistoryController
+);
+
+router.post("/watchlist",
+    [
+        body("movieId", "Movie Id required").exists(),
+        header("Authorization", "Authorization token is required").exists()
+    ],
+    (req, res, next) => {
+        const errors = validationResult(req);
+        console.log(errors);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    },
+    isLoggedIn, updateWatchlistController
+);
+
+router.delete("/watchlist",
+    [
+        body("movieId", "Movie Id required").exists(),
+        header("Authorization", "Authorization token is required").exists()
+    ],
+    (req, res, next) => {
+        const errors = validationResult(req);
+        console.log(errors);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    },
+    isLoggedIn, deleteWatchlistController
+);
