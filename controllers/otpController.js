8e262f7c516aadb1bb7b8d0eaf18c000df574db3
Vivek79781 <vivekdbz248@gmail.com>
2024-03-22 12:31:50 +0530

    OTP Done

--- controllers/otpController.js
+++ controllers/otpController.js
@@ -1,183 +1,166 @@
 const message = require("../utils/message");
 const sendingMail = require("../utils/mailer");
-const jwt = require("jsonwebtoken");
 const prisma = require("../prisma/index");
 const bcrypt = require("bcryptjs");
 
+const { generateJWT, parseToken } = require("../utils/token");
+
+const generateOtpPhoneNumber = async (req) => {
+  let otp = Math.floor(100000 + Math.random() * 900000);
+  let phone = req.body.phoneNumber;
+  console.log(phone);
+  message({
+    to: phone,
+    body: `Your OTP is ${otp}`,
+  });
+  const salt = await bcrypt.genSalt(10);
+  otp = await bcrypt.hash(`${otp}`, salt);
+  const token = generateJWT({ otp, phoneNumber: phone });
+  console.log("Controller", token);
+  return token
+}
+
+const generateOtpEmail = async (req) => {
+  let otp = Math.floor(100000 + Math.random() * 900000);
+  const email = req.body.email;
+  const token = parseToken(req);
+  const userId = token.userId;
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+  if(!user){
+    return res.status(400).json({ message: "Mobile number not verified" });
+  }
+  sendingMail({
+    from: "no-reply@example.com",
+    to: email,
+    subject: "OTP",
+    text: `Your OTP is ${otp}`,
+  });
+
+  const salt = await bcrypt.genSalt(10);
+  otp = await bcrypt.hash(`${otp}`, salt);
+
+  const newToken = generateJWT({ otp, userId, email });
+  return newToken
+}
 
 const generateOtp = async (req, res) => {
-
   try {
-    if (!req.body || !(req.body.email || req.body.phoneNumber)) {
-      return res
-        .status(400)
-        .json({ message: "Please provide a phone number or email" });
-    }
-
-    // if (req.body.type !== "phoneNumber" && req.body.type !== "email") {
-    //   return res.status(400).json({ message: "Please provide a valid type" });
-    // }
-
-    let otp = Math.floor(100000 + Math.random() * 900000);
-
-    let tokenBody;
-
-    const type = req.body.email ? "email" : "phoneNumber";
-
-    if (type === "phoneNumber") {
-      let phone = req.body.phoneNumber;
-
-      //send the otp to the user
-      message({
-        to: phone,
-        body: `Your OTP is ${otp}`,
+    const { email, phoneNumber } = req.body;
+    if(phoneNumber){
+      const token = await generateOtpPhoneNumber(req, res);
+      console.log("Generated OTP", token);
+      return res.status(200).json({
+        message: "OTP sent successfully",
+        data: {
+          token: token,
+        },
       });
-
-      const salt = await bcrypt.genSalt(10);
-      otp = await bcrypt.hash(`${otp}`, salt);
-
-      console.log(otp);
-
-      tokenBody = { otp };
-    } else if (type == "email") {
-      let email = req.body.email;
-
-      if (!req.body.token) {
-        return res.status(400).json({
-          message: "Please provide a token for the email verification",
-        });
-      }
-
-      let token = req.body.token;
-      const userId = jwt.verify(
-        token,
-        process.env.JWT_SECRET || "seroweuhnclkhvasouae",
-      ).userId;
-
-      //send the otp to the user
-      sendingMail({
-        from: "no-reply@example.com",
-        to: email,
-        subject: "OTP",
-        text: `Your OTP is ${otp}`,
+    } else {
+      const token = await generateOtpEmail(req, res);
+      return res.status(200).json({
+        message: "OTP sent successfully",
+        data: {
+          token: token,
+        },
       });
-
-      const salt = await bcrypt.genSalt(10);
-      otp = await bcrypt.hash(`${otp}`, salt);
-
-      tokenBody = { otp, userId };
     }
-
-    // use jwt to generate a token
-    const token = jwt.sign(
-      tokenBody,
-      process.env.JWT_SECRET || "seroweuhnclkhvasouae",
-      { expiresIn: "10m" },
-    );
-
-    return res.status(200).json({
-      message: "OTP sent successfully",
-      data: {
-        token: token,
-      },
-    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "Internal server error" });
   }
 };
 
+const verifyOtpEmail = async req => {
+  const otp = parseInt(req.body.otp);
+  const token = parseToken(req);
+  const userId = token.userId;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+
+  if (!user) {
+    return res.status(400).json({ message: "Invalid user" });
+  }
+
+  const isMatch = await bcrypt.compare(`${otp}`, token.otp);
+
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid OTP" });
+  }
+
+  const updatedUser = await prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      email: token.email,
+    },
+  });
+
+  const newToken = generateJWT({ userId });
+  return newToken
+}
+
+const verifyOtpPhoneNumber = async req => {
+  const otp = parseInt(req.body.otp);
+  const token = parseToken(req);
+
+  const isMatch = await bcrypt.compare(`${otp}`, token.otp);
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid OTP" });
+  }
+  const findUser = await prisma.user.findFirst({
+    where: {
+      phone: token.phoneNumber,
+    },
+  });
+  if(findUser){
+    if(findUser.password){
+      return { msg: "User already exists" }
+    }
+    const deletedUser = await prisma.user.delete({
+      where: {
+        id: findUser.id,
+      },
+    });
+  }
+  const user = await prisma.user.create({
+    data: {
+      phone: token.phoneNumber,
+    },
+  });
+
+  const newToken = generateJWT({ userId: user.id });
+  return { token: newToken }
+}
+
 const verifyOtp = async (req, res) => {
   try {
-    if (!req.body || !(req.body.email || req.body.phoneNumber)) {
-      return res
-        .status(400)
-        .json({ message: "Please provide an otp and token" });
-    }
-
-    const otp = parseInt(req.body.otp);
-    const token = req.body.token;
-
-    // compare the otp using jwt
-    const decoded = jwt.verify(
-      token,
-      process.env.JWT_SECRET || "seroweuhnclkhvasouae",
-    );
-
-    if (!decoded) {
-      return res.status(400).json({ message: "Invalid token" });
-    }
-
-    console.log(decoded);
-
-    const isMatch = await bcrypt.compare(`${otp}`, decoded.otp);
-
-    if (!isMatch) {
-      return res.status(400).json({ message: "Invalid OTP" });
-    }
-
-    const type = req.body.email ? "email" : "phoneNumber";
-
-    if (type === "phoneNumber") {
-      const user = await prisma.User.create({
-        data: {
-          phone: {
-            countryCode: req.body.phoneNumber.substring(0, 3),
-            phoneNumber: req.body.phoneNumber.substring(3),
-          },
-        },
-      });
-
-      // get the user id
-      const userId = user.id;
-
-      // encrypt the user id using jwt
-      const token = jwt.sign(
-        { userId },
-        process.env.JWT_SECRET || "seroweuhnclkhvasouae",
-        { expiresIn: "1h" },
-      );
-
+    const token = parseToken(req);
+    if(token.phoneNumber){
+      const { token, msg } = await verifyOtpPhoneNumber(req, res);
+      if(msg){
+        return res.status(400).json({ message: msg });
+      }
       return res.status(200).json({
         message: "OTP verified successfully",
         data: {
           token: token,
         },
       });
-    } else if (type == "email") {
-      // find the user from the database by decrypting the token
-      const userId = decoded.userId;
-      const user = await prisma.user.findUnique({
-        where: {
-          id: userId,
-        },
-      });
-
-      if (!user) {
-        return res.status(400).json({ message: "Invalid user" });
-      }
-
-      // add the email to the user
-      const updatedUser = await prisma.User.update({
-        where: {
-          id: userId,
-        },
-        data: {
-          email: req.body.email,
-        },
-      });
-
-      // encrypt the user id using jwt
-      const newToken = jwt.sign(
-        { userId },
-        process.env.JWT_SECRET || "seroweuhnclkhvasouae",
-        { expiresIn: "1h" },
-      );
-
+    } else {
+      const token = await verifyOtpEmail(req, res);
       return res.status(200).json({
         message: "OTP verified successfully",
         data: {
-          token: newToken,
+          token: token,
         },
       });
     }
@@ -185,7 +168,7 @@ const verifyOtp = async (req, res) => {
     console.log(error);
     return res.status(500).json({ message: "Internal server error" });
   }
-};
+}
 
 module.exports = {
   generateOtp,
--- routes/otpRoutes.js
+++ routes/otpRoutes.js
@@ -4,14 +4,19 @@ const { generateOtp, verifyOtp } = require("../controllers/otpController");
 
 
 
-const { body, validationResult } = require('express-validator');
+const { body, header, oneOf, validationResult, withMessage } = require("express-validator");
+const message = require("../utils/message");
 
 // Middleware function to validate email or phoneNumber
 const validateEmailOrPhoneNumber = [
   // Check if email or phoneNumber is provided
-  body('credential').optional().isEmail().normalizeEmail(),
-  body('credential').optional().isLength(13).withMessage('Invalid phone number'),
-
+  oneOf([
+    body("phoneNumber",).exists().isMobilePhone(),
+    [
+      body("email").exists().isEmail(),
+      header("Authorization").exists()
+    ]
+  ], { message: "Email with Authentication or phoneNumber is required" }),
   // Check for validation errors
   (req, res, next) => {
     const errors = validationResult(req);
@@ -23,38 +28,18 @@ const validateEmailOrPhoneNumber = [
   }
 ];
 
-
-// Middleware function to validate email or phoneNumber based on type
-// const validateCredential = [
-//   // Check if type is provided and valid
-//   body('type').notEmpty().isIn(['email', 'phoneNumber']).withMessage('Invalid type'),
-
-//   // Dynamically validate credential based on type
-//   (req, res, next) => {
-//     const { type, credential } = req.body;
-
-//     // Validate credential based on type
-//     let validationRule;
-//     if (type === 'email') {
-//       validationRule = body('credential').isEmail().normalizeEmail();
-//     } else if (type === 'phoneNumber') {
-//       validationRule = body('credential').isMobilePhone().withMessage('Invalid phone number');
-//     }
-
-//     // Check for validation errors
-//     validationRule(req, res, () => {
-//       const errors = validationResult(req);
-//       if (!errors.isEmpty()) {
-//         return res.status(400).json({ errors: errors.array() });
-//       }
-//       next();
-//     });
-//   }
-// ];
-
-
 router.post("/generate", validateEmailOrPhoneNumber, generateOtp);
-router.post("/verify", verifyOtp);
+router.post("/verify", [
+  body("otp", "OTP is required").exists().isNumeric(),
+  header("Authorization", "Authorization token is required").exists()
+], (req, res, next) => {
+  const errors = validationResult(req);
+  console.log(errors);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+}, verifyOtp);
 
 module.exports = router;
 
