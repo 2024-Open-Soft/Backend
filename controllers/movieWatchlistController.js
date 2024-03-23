77a04aa366fad8def199216427169458c35c7469
Uday <udayomsrivastava111@gmail.com>
2024-03-23 15:47:11 +0530

    movie watchlist mongoose

--- controllers/movieWatchlistController.js
+++ controllers/movieWatchlistController.js
@@ -1,51 +1,47 @@
 const User = require("../models/user");
 
 const updateWatchlistController = async (req, res) => {
-  try {
-    const { movieId } = req.body;
-    const user = req.user;
+	try {
+		const { movieId } = req.body;
+		const user = req.user;
 
-    if (user.watchLaterIds.includes(movieId))
-      return res.status(400).json({ message: "already exists in watchlist" });
+		if (user.watchLaterIds.includes(movieId))
+			return res.status(400).json({ message: "already exists in watchlist" });
 
-    await User.findByIdAndUpdate(user.id, {
-      $push: { watchLaterIds: movieId },
-    })
+		await User.findByIdAndUpdate(user.id, {
+			$push: { watchLaterIds: movieId },
+		})
 
-    return res.status(200).json({ message: "Watchlist updated" });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: "Internal server error" });
-  }
+		return res.status(200).json({ message: "Watchlist updated" });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ message: "Internal server error" });
+	}
 };
 
 const deleteWatchlistController = async (req, res) => {
-  try {
-    const { movieId } = req.body;
+	try {
+		const { movieId } = req.body;
 
-    const user = req.user;
+		const user = req.user;
 
-    if (!user.watchLaterIds.includes(movieId))
-      return res.status(400).json({ message: "does not exist in watchlist" });
+		if (!user.watchLaterIds.includes(movieId))
+			return res.status(400).json({ message: "does not exist in watchlist" });
 
-    await prisma.user.update({
-      where: {
-        id: user.id,
-      },
-      data: {
-        watchLaterIds: user.watchLaterIds.filter((mId) => mId != movieId),
-      },
-    });
 
-    return res.status(200).json({ message: "Watchlist updated" });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: "Internal server error" });
-  }
+		await User.findByIdAndUpdate(user.id, {
+			$pull: { watchLaterIds: movieId },
+		})
+
+		return res.status(200).json({ message: "Watchlist updated" });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ message: "Internal server error" });
+	}
 };
 
 module.exports = {
-  updateWatchlistController,
-  deleteWatchlistController,
+	updateWatchlistController,
+	deleteWatchlistController,
 };
 
