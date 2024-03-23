d5d3faa3d84c3ba6be3aab3c7dca7dfd6d1fbb7d
Uday <udayomsrivastava111@gmail.com>
2024-03-23 16:50:15 +0530

    userRoutes and userController

--- index.js
+++ index.js
@@ -10,17 +10,22 @@ const PORT = 3001;
 const otpRoutes = require("./routes/otpRoutes");
 const register_routes = require("./routes/register_routes");
 const loginRoutes = require("./routes/loginRoutes");
-// const comment_routes = require('./routes/comment_route')
 const movieRoutes = require("./routes/movieRoutes");
 const adminRoutes = require("./routes/adminRoutes");
+const commentRoutes = require("./routes/commentRoutes");
 
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use("/user", register_routes);
 app.use("/user", loginRoutes);
+app.use("/user", userRoutes);
+
 app.use("/otp", otpRoutes);
+
 app.use("/movie", movieRoutes);
-// app.use("/admin", adminRoutes);
+app.use("/movie", commentRoutes);
+
+app.use("/admin", adminRoutes);
 
 async function main() {
   await mongoose.connect(process.env.DATABASE_URL);
