2a1dd932d37bc06d1b80d9ed571c87016009ea33
Uday <udayomsrivastava111@gmail.com>
2024-03-27 23:42:48 +0530

    replacing ids with actual data in some apis

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,36 @@
-const { User } = require("../models");
+const { User, Movie, Comment } = require("../models");
 
 const getProfile = async (req, res) => {
   try {
     let user = req.user;
+
+    const watchLater = [], history = [], comments = [];
+    
+    for(let i in user.watchLater)
+    {
+      console.log(i)
+      // user.watchLater[i] = await Movie.findById(user.watchLater[i]);
+      watchLater[i] = await Movie.findById(user.watchLater[i]);
+    }
+
+    for(let i in user.history)
+    {
+      history[i] = {movie: user.history[i].movie, timeStamp: user.history[i].timeStamp};
+      history[i].movie = await Movie.findById(user.history[i].movie);
+    }
+
+    for(let i in user.comments)
+    {
+      comments[i] = await Comment.findById(user.comments[i]);
+    }
+
     return res.status(200).json({
       message: "Profile fetched successfully",
       data: {
-        user,
+        ...user.toObject(),
+        watchLater,
+        history,
+        comments,
       },
     });
   } catch (error) {
