930c8720c69b8e726228f6e8b77add0a8ec3f224
Sagnibha <sagnibha123@gmail.com>
2024-03-28 11:23:48 +0530

    get active subscription plan done

--- controllers/plan.js
+++ controllers/plan.js
@@ -1,5 +1,8 @@
 const { SubscriptionPlan } = require('../models');
 
+
+
+
 const getSubscriptionPlans = async (req, res) => {
     try {
         const plans = await SubscriptionPlan.find();
@@ -24,5 +27,50 @@ const getSubscriptionPlan = async (req, res) => {
     }
 }
 
+const getActiveSubscriptionPlan=async(req,res)=>{
 
-module.exports = { getSubscriptionPlans, getSubscriptionPlan };
+    try{
+        const user_data=req.user;
+
+        if(!user_data)
+        {
+            return res.status(404).send("User not found")
+        }
+
+        for(const i of user_data.subscriptions)
+        {
+            const currentdate = new Date();
+            const startDate=new Date(i.startDate);
+            const duration = i.orignalDuration;
+
+            const endDate = new Date(startDate.getTime() + duration *30* 24 * 60 * 60 * 1000);
+
+            console.log(endDate);
+
+            console.log(startDate, currentdate, startDate<=currentdate);
+
+            if(endDate>=currentdate && startDate<=currentdate)
+            {
+                const plan = await SubscriptionPlan.findById(i.plan);
+                return res.json({
+                    plan:plan,
+                    status:i.status,
+                    startDate:startDate,
+                    endDate:endDate
+                });
+            }
+
+        }
+        return res.status(404).send("No active plan found");
+    }
+    catch(error)
+    {
+        console.log(error)
+        return res.status(500).send("Internal server error");
+    }
+}
+
+
+
+
+module.exports = { getSubscriptionPlans, getSubscriptionPlan,getActiveSubscriptionPlan };
--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,7 @@ const register = require("../controllers/register");
 const { loginUser } = require("../controllers/login");
 const { validate } = require("../utils/validator");
 
+
 router.post(
   "/register", // Register user endpoint
   [
@@ -38,5 +39,7 @@ router.post(
 router.get("/profile", isLoggedIn, getProfile);
 
 router.put("/profile", isLoggedIn, updateProfile);
+
+
 
 module.exports = router;
