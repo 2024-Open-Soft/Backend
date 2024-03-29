66cbcebeaad4989f1f1f324f6f1b973ebc276044
Uday <udayomsrivastava111@gmail.com>
2024-03-30 02:30:59 +0530

    admin user crud routes

--- controllers/admin-user.js
+++ controllers/admin-user.js
@@ -117,8 +117,7 @@ const deleteUser = async (req, res) => {
     try {
         const { id } = req.params;
 
-        const user
-        await User.findByIdAndDelete(id);
+        const user = await User.findByIdAndDelete(id);
 
         return res.status(200).json({
             data: {
