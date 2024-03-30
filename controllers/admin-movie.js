5236dc6ed39a76bb51974a15028a443734b84d15
Jagteshver Singh <85994817+JS-CodeGamer@users.noreply.github.com>
2024-03-30 23:24:15 +0530

    poster upload correction

--- controllers/admin-movie.js
+++ controllers/admin-movie.js
@@ -60,7 +60,7 @@ async function uploadMovieFile(req, res) {
     await aws.convertVideo(
       `movies/${movie._id}/original`,
       [360, 720, 1080],
-      `movies/${movie._id}/`
+      `movies/${movie._id}/`,
     );
   } catch (e) {
     return res.status(500).json({ error: "error converting video" });
@@ -114,7 +114,7 @@ async function uploadTrailer(req, res) {
     await aws.convertVideo(
       `trailers/${movie._id}/original`,
       [1080],
-      `trailers/${movie._id}/`
+      `trailers/${movie._id}/`,
     );
   } catch (e) {
     return res.status(500).json({ error: "error converting video" });
@@ -153,7 +153,7 @@ async function uploadPoster(req, res) {
   }
 
   await Movie.findByIdAndUpdate(movie._id, {
-    poster: aws.getS3Url(`posters/${movie._id}/original-1080.m3u8`),
+    poster: aws.getS3Url(`posters/${movie._id}`),
   });
 
   return res.json({ message: "success" });
